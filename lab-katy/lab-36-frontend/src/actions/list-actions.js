a9c38e45a07c18b76a427fc4a6f415e21ad2359d
Katy Robinson <katyorob@gmail.com>
2018-04-02 14:33:51 -0700

    update action creator/others added

---
+++ lab-katy/lab-36-frontend/src/actions/list-actions.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import superagent from 'superagent';
+
+export const listSet = lists => ({
+  type: 'LIST_SET', 
+  payload: lists,
+})
+
+export const listCreate = list => ({
+  type: 'LIST_CREATE', 
+  payload: list,
+})
+
+export const listUpdate = list => ({
+  type: 'LIST_UPDATE', 
+  payload: list,
+})
+
+export const listDestroy = list => ({
+  type: 'LIST_DESTROY',
+  payload: list,
+})
+
+export const listFetchRequest = () => (dispatch) => {
+  return superagent.get(`${__API_URL__}/api/lists`)
+  .then( res => {
+    dispatch(listSet(res.body));
+    return res;
+  })
+}
+
+export const listCreateRequest = list => dispatch => {
+  return superagent.post(`${__API_URL__}/api/lists`)
+  .send(list)
+  .then( res => {
+    dispatch(listCreate(res.body));
+    return res;
+  })
+}
+
+export const listUpdateRequest = list => dispatch => {
+  return superagent.put(`${__API_URL__}/api/lists/${list.id}`)
+  .then( res => {
+    dispatch(listUpdate(list));
+    return res;
+  })
+}
+
+export const listDestroyRequest = list => dispatch => {
+  return superagent.delete(`${__API_URL__}/api/lists/${list.id}`)
+  .then( res => {
+    dispatch(listDestroy(list));
+    return res;
+  })
+}
